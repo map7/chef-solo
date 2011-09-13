c3163af368ce48b4a4a2553ef492f9c52fb8c7bb
Michael Pope <map7777@gmail.com>
2011-09-13 17:12:23 +1000

    Compile rubygems - this fixes the path problems i've been having

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # This runs as root on the server
-chef_binary=/var/lib/gems/1.9.1/bin/chef-solo
+#chef_binary=/var/lib/gems/1.9.1/bin/chef-solo
+chef_binary=/usr/bin/chef-solo
 
 # Are we on a vanilla system?
 if ! test -f "$chef_binary"; then
@@ -13,8 +14,16 @@ if ! test -f "$chef_binary"; then
          --force-yes -fuy dist-upgrade
     
      # Install Ruby & Chef
-     aptitude install -y ruby1.9.1 ruby1.9.1-dev make rubygems1.9.1 libopenssl-ruby1.9.1 
-     sudo gem1.9.1 install --no-rdoc --no-ri chef --version 0.10.0
+     aptitude install -y ruby1.9.1 ruby1.9.1-dev make libopenssl-ruby1.9.1 
+
+     # Install rubygems
+     cd /tmp
+     wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.10.tgz
+     tar zxf rubygems-1.8.10.tgz
+     cd rubygems-1.8.10
+     sudo ruby1.9.1 setup.rb --no-format-executable 
+
+     sudo gem install --no-rdoc --no-ri chef --version 0.10.0
 fi
 
 "$chef_binary" -c solo.rb -j solo.json
