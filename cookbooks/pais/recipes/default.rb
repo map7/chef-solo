59d8ba590959da69a0a9cc59ee958800c65ed0c8
Michael Pope <map7777@gmail.com>
2011-09-26 17:05:32 +1000

    Setup apache with passenger lines in a config file

--- cookbooks/pais/recipes/default.rb
+++ cookbooks/pais/recipes/default.rb
@@ -7,3 +7,21 @@ package 'screen'
 gem_package "rails" do
   action :install
 end
+
+# --- Setup Apache2
+# The passenger-apache cookbook has compiled the library all you need to do is 
+# include the lines in your httpd.conf file.
+file "/etc/apache2/httpd.conf" do
+  owner "root"
+  group "root"
+  mode "0644"
+  backup 1
+  action :create
+  content "
+LoadModule passenger_module /usr/local/rvm/gems/ruby-1.9.2-p290/gems/passenger-3.0.7/ext/apache2/mod_passenger.so
+PassengerRoot /usr/local/rvm/gems/ruby-1.9.2-p290/gems/passenger-3.0.7
+PassengerRuby /usr/local/rvm/wrappers/ruby-1.9.2-p290/ruby
+"
+end
+
+
