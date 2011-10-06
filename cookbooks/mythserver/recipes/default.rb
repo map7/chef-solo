c410c3eef2341c38f106d2e092c31dbee7cdb869
Michael Pope <map7777@gmail.com>
2011-10-06 21:30:24 +1100

    Added remote config files

--- cookbooks/mythserver/recipes/default.rb
+++ cookbooks/mythserver/recipes/default.rb
@@ -1,3 +1,11 @@
+# Mythtv Server setup
+#
+# Currently this assumes you are:
+# - Running Mythbuntu 11.04 or greater.
+# - Using a MCE compatible remote.
+# - Live in Australia (shepherd EPG)
+#
+
 # --- Install system tools
 package 'sysstat'
 package 'htop'
@@ -17,6 +25,12 @@ package 'gnome-do'
 # Install remote control packages
 package 'irda-utils'
 package 'xmacro'
+
+cookbook_file "#{ENV['HOME']}/.lirc/mythtv" do
+  source "mythtv"
+  backup 2
+  mode "0644"
+end
 
 # Shepherd requirements
 %w[xmltv libxml-simple-perl libalgorithm-diff-perl libgetopt-mixed-perl libcompress-zlib-perl libdata-dumper-simple-perl libdate-manip-perl liblist-compare-perl libdatetime-format-strptime-perl libhtml-parser-perl libxml-dom-perl libgd-gd2-perl libdigest-sha1-perl libarchive-zip-perl libio-string-perl libdbi-perl].each do |pkg|
