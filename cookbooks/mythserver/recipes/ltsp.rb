ddd638a550166b66ee06517dc109d8b453fbe69c
Michael Pope <map7777@gmail.com>
2012-01-25 13:39:58 +1100

    Add a script for ltsp_chroot Add building diskless server command

--- cookbooks/mythserver/recipes/ltsp.rb
+++ cookbooks/mythserver/recipes/ltsp.rb
@@ -1,9 +1,18 @@
 # Mythtv Server LTSP Setup
 #
 #
+home = ENV['HOME']
+user = ENV['SUDO_USER']
 
 package 'dhcp3-server'
 package 'tftp'
+
+# Copy diskless readme
+cookbook_file "/home/mythserver/Desktop/diskless_readme.txt" do
+  source "diskless_readme.txt"
+  owner "mythserver"
+  group "mythserver"
+end
 
 # Static IP settings
 cookbook_file "/etc/network/interfaces" do
@@ -19,4 +28,21 @@ cookbook_file "/etc/dhcp/dhcpd.conf" do
   backup 5
   owner "root"
   group "root"
+end
+
+# Install diskess server
+package 'mythbuntu-diskless-server'
+
+execute "Create diskless image" do
+  command 'ltsp-build-client --mythbuntu --mythbuntu-user-credentials "mythclient":"mythclient" --copy-package-lists --copy-sourceslist --accept-unsigned-packages --arch i386'
+  action :run
+end
+
+# Add ltsp_chroot
+cookbook_file "#{home}/bin/ltsp_chroot" do
+  source "scripts/ltsp_chroot"
+  backup 2
+  mode "0755"
+  owner user
+  group user  
 end
