0c7b61f0205b5aca4324fed6c991f7573a9844b6
Michael Pope <map7@corsairsolutions.com.au>
2012-07-29 20:29:20 +1000

    Changed bootstrap over to installing ruby from source

--- install.sh
+++ install.sh
@@ -14,15 +14,15 @@ if ! test -f "$chef_binary"; then
      export DEBIAN_FRONTEND=noninteractive
 
      # Upgrade headlessly (this is only safe-ish on vanilla systems)
-     # apt-get update -o Acquire::http::No-Cache=True
-     # apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
+     apt-get update -o Acquire::http::No-Cache=True
+     apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
 
      apt-get install -y curl build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev git-core bzip2
 
 	 # Download ruby 1.9.3-p125
 	 cd /tmp
-	 # wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p125.tar.gz
-	 # tar -xvzf ruby-1.9.3-p125.tar.gz
+	 wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p125.tar.gz
+	 tar -xvzf ruby-1.9.3-p125.tar.gz
 
 	 # Install
 	 cd ruby-1.9.3-p125/
@@ -32,6 +32,9 @@ if ! test -f "$chef_binary"; then
      
      # Install chef
      gem install chef ruby-shadow --no-rdoc --no-ri
+
+	 # Return to the chef directory
+	 cd ~/chef
 fi
 
 # Run chef-solo on server
